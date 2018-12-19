f2a5a63464937a364f0d20458ec20f39f41eee72
willy0001 <43977894+willy0001@users.noreply.github.com>
2018-12-19 15:08:25 +0800

    Updata12/19.02
    Stop do the loadPMjson()

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ def get_search_engine(search_thing, result_num=4):
 import os
 if __name__ == "__main__":
     # load PM2.5 records
-    loadPMJson()
+    #loadPMJson()
     
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
